0d5b6d2a268acf0e7bd190f6c847a487c03c68b2
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2022-12-28 05:59:12 +0100

    multiple return

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,49 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
-const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
-};
+import React, { useState, useEffect } from 'react'
+const url = 'https://api.github.com/users/Demandtech'
 
-export default MultipleReturns;
+const MultipleReturns = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+  const [user, setUser] = useState('default user')
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetch(url)
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 299) {
+          setIsLoading(false)
+          return resp.json()
+        }else {
+          setIsLoading(false)
+          setIsError(true)
+          throw new Error(resp.statusText)
+        }
+      })
+      .then((user) => {
+        const { login } = user
+        console.log(user)
+        setUser(login)
+        setIsLoading(false)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    )
+  }
+  return <h2>{user}</h2>
+}
+
+export default MultipleReturns
