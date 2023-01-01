19e5fe9a490789c38fa58d790e6b069b08f06a61
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-01-01 08:02:56 +0100

    show-hide component

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,17 +4,21 @@ import React, { useState } from 'react'
 
 const ShortCircuit = () => {
   const [text, setText] = useState('')
-  const firstValue = text || 'hello world'
-  const secondValue = text && 'hello world'
-  console.log(secondValue)
+  // const firstValue = text || 'hello world'
+  // const secondValue = text && 'hello world'
+  const [isError, setIsError] = useState(false)
+  
   
   return (
     <>
       {/* <h1>{firstValue}</h1>
       <h1>value: {secondValue}</h1> */}
       <h1>{text || 'John Doe'}</h1>
-      {text && <h1>hello World</h1>}
-      {!text && <h1>hello World</h1>}
+      <button onClick={()=> setIsError(!isError)} className='btn'>toggle error</button>
+      {isError && <h1>Error...</h1>}
+      {isError ? (<p>there is an error</p>) : (<div>
+        <h2>there is no error</h2>
+      </div>)}
     </>
   ) 
 }
