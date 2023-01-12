243ed381d4db7572342cd4f880f399ce5f241379
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-01-12 12:28:26 +0100

    props-drilling

--- src/tutorial/7-prop-drilling/final/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/final/1-prop-drilling.js
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
-import { data } from '../../../data';
+import React, { useState } from 'react'
+import { data } from '../../../data'
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  const [people, setPeople] = useState(data);
+  const [people, setPeople] = useState(data)
   const removePerson = (id) => {
     setPeople((people) => {
-      return people.filter((person) => person.id !== id);
-    });
-  };
+      return people.filter((person) => person.id !== id)
+    })
+  }
   return (
     <section>
       <h3>prop drilling</h3>
       <List people={people} removePerson={removePerson} />
     </section>
-  );
-};
+  )
+}
 
 const List = ({ people, removePerson }) => {
   return (
@@ -28,11 +28,11 @@ const List = ({ people, removePerson }) => {
             {...person}
             removePerson={removePerson}
           />
-        );
+        )
       })}
     </>
-  );
-};
+  )
+}
 
 const SinglePerson = ({ id, name, removePerson }) => {
   return (
@@ -40,7 +40,7 @@ const SinglePerson = ({ id, name, removePerson }) => {
       <h4>{name}</h4>
       <button onClick={() => removePerson(id)}>remove</button>
     </div>
-  );
-};
+  )
+}
 
-export default PropDrilling;
+export default PropDrilling
