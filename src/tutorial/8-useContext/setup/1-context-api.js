f7a5e25c19cca6ec4b29d143b2b222f7ed2caf05
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-01-15 01:56:33 +0100

    custom hook

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -1,46 +1,50 @@
-import React, { useState, useContext } from 'react';
-import { data } from '../../../data';
+import React, { useState, useContext } from 'react'
+import { data } from '../../../data'
 // more components
 // fix - context api, redux (for more complex cases)
 
+const PersonContext = React.createContext()
+//two components - Provider and Consumer
+
+
+
 const ContextAPI = () => {
-  const [people, setPeople] = useState(data);
+  const [people, setPeople] = useState(data)
+  
   const removePerson = (id) => {
     setPeople((people) => {
-      return people.filter((person) => person.id !== id);
-    });
-  };
+      return people.filter((person) => person.id !== id)
+    })
+  }
   return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
-  );
-};
+    <PersonContext.Provider value={{removePerson, people}}>
+      <h3>Context Api</h3>
+      <List people={people}/>
+    </PersonContext.Provider>
+  )
 
-const List = ({ people, removePerson }) => {
+}
+
+const List = ({ people}) => {
+  const mainData = useContext(PersonContext)
+  console.log(mainData)
   return (
     <>
       {people.map((person) => {
-        return (
-          <SinglePerson
-            key={person.id}
-            {...person}
-            removePerson={removePerson}
-          />
-        );
+        return <SinglePerson key={person.id} {...person} />
       })}
     </>
-  );
-};
+  )
+}
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  const { removePerson } = useContext(PersonContext)
   return (
     <div className='item'>
       <h4>{name}</h4>
       <button onClick={() => removePerson(id)}>remove</button>
     </div>
-  );
-};
+  )
+}
 
-export default ContextAPI;
+export default ContextAPI
